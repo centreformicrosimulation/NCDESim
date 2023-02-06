babb9039bf9289fae61abe62ceed27b38ad68f7f
pbronka <56582427+pbronka@users.noreply.github.com>
2023-02-06 15:29:51 +0000

    Update FirmRemovalFilter.java
    Clean up and document FirmRemovalFilter

--- src/main/java/NCDESim/data/filters/FirmRemovalFilter.java
+++ src/main/java/NCDESim/data/filters/FirmRemovalFilter.java
@@ -3,14 +3,32 @@ package NCDESim.data.filters;
 import org.apache.commons.collections4.Predicate;
 import NCDESim.model.AbstractFirm;
 
-
+/**
+ * Class for removing firms based on certain criteria.
+ *
+ * @param <T> The type of firm to filter, must extend the AbstractFirm class.
+ */
 public class FirmRemovalFilter<T extends AbstractFirm> implements Predicate<T> {
 
-    // Firms will be removed when the filter evaluates to true.
+    /**
+     * Evaluates a firm to determine if it should be removed.
+     * Firms will be removed if either of the following conditions is true:
+     *  - The firm has no employees.
+     *  - The firm's profit is less than or equal to zero.
+     *
+     * @param firm The firm to evaluate.
+     * @return True if the firm should be removed, false otherwise.
+     * @throws IllegalArgumentException If the firm argument is null.
+     */
     @Override
-    public boolean evaluate(T t) {
-        return ((t.getEmployeesSet().size() == 0) // Firms with no employees are removed
-        //        (t.getProfit() <= -3) // Firms with no profits are removed
+    public boolean evaluate(T firm) {
+        if (firm == null) {
+            throw new IllegalArgumentException("The firm argument cannot be null.");
+        }
+
+        return ((firm.getEmployeesSet().size() == 0)  // Firms with no employees are removed
+                || (firm.getProfit() <= 0) // Firms with no profits are removed
         );
     }
 }
+
